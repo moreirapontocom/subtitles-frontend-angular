e70d39b49e714383a5aced9f6a8bb00c26ddfbe6
Lucas Moreira <moreirapontocom@gmail.com>
2021-12-22 14:54:54 -0300

    Production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api: "http://167.71.162.181",
+  api: "https://captions-api.lucasmoreira.com.br",
   client: "https://captions.lucasmoreira.com.br",
   youtubeApiKey: "AIzaSyC-FhFJq7JYcqio6AKMSCpxmMXcUBrDg7o",
   youtubeApiClientId: "696404688388-efcv0009novvt7c7mf6pjak2vs5hrlsk.apps.googleusercontent.com",
